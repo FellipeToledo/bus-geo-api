b31149f7527f0e45d315e027f7e5044b62edcc93
Fellipe Toledo <41087162+FellipeToledo@users.noreply.github.com>
2025-03-20 10:46:15 -0300

    feat(setup): configura projeto Spring Boot com dependências e PostGIS (#2)
    Co-authored-by: Fellipe Toledo de Azevedo <10598925708@rio.rj.gov.br>

---
+++ src/main/java/com/azvtech/bus_geo_api/BusGeoApiApplication.java
@@ -0,0 +1,13 @@
+package com.azvtech.bus_geo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusGeoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusGeoApiApplication.class, args);
+	}
+
+}
